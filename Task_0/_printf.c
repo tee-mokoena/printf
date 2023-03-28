95c7d7f21fefcee1a4bbfb8fd4b146a3e8f4fffe
Tebogo <122821010+tee-mokoena@users.noreply.github.com>
2023-03-28 22:26:46 +0200

    Update _printf.c
    Only include the "main.h" file
    Separated the character and strings functions.

--- Task_0/_printf.c
+++ Task_0/_printf.c
@@ -1,25 +1,32 @@
 #include "main.h"
-#include <unistd.h>
-#include <string.h>
-#include <stdarg.h>
+
+/* print a character */
 
 int _printf(const char *format, ...)
 {
 	char c;
-    	char *string;
-    	int length;
     	va_list ap;
     	va_start(ap, format);
-    	if (format == "%s")
+    	if (format == "%c")
 	{
-        string = va_arg(ap, char *);
-        length = strlen(string) + 1;
-        return(write(1, string, length));
-    	}
- 	else if (format == "%c")
-	{
-        c = va_arg(ap, int);
-        return(write(1, c, 1));
-    	}
+		c = va_arg(ap, int);
+		return (write(1, c, 1));
+	}
 	va_end(ap);
-}
+	
+/* print a string */
+		
+ int _printf(const char *format, ...)
+ {
+	 char *str = va_arg(ap, char *);
+	 
+	 int length = 0;
+	 
+	 while (str[length] != '\0')
+		 length++;
+	 	 
+	 if (format == "%s")
+	 {
+		 length = _strlen(str) + 1;
+		 return (write(1, str, 1));
+	 }
