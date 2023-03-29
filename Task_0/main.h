49a1a46b09906a38f44c095dd2a1a8fd0eb9ad19
TeeM <teemokoena@gmail.com>
2023-03-29 12:23:34 -0700

    printf

--- Task_0/main.h
+++ Task_0/main.h
@@ -27,12 +27,13 @@ struct fmt
 {
 	char fmt;
 	int (*fn)(va_list, char[], int, int, int, int);
-}
+};
 
 /**
  * typedef struct fmt fmt_t - Struct op
- * @fmt: the format
- * @fm_t: the function associated
+ *
+ * @fmt: The format.
+ * @fm_t: The function associated.
  */
 typedef struct fmt fmt_t;
 
